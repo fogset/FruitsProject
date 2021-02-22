6536722b4f2cfe9282bf9a618beebd30a25b6f69
fogset <zhangth1990@hotmail.com>
2021-02-22 05:38:13 -0800

    starting with mongoose

--- app.js
+++ app.js
@@ -1,54 +1,26 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+const mongoose = require('mongoose');
 
-// Database Name
-const dbName = 'myproject';
+mongoose.connect("mongodb://localhost:27017/myproject", { useNewUrlParser:true});
 
-// Create a new MongoClient
-const client = new MongoClient(url, {
-  useUnifiedTopology: true
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String
 });
 
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-  const db = client.db(dbName);
-
-  findDocuments(db, function() {
-     client.close();
-   });
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit."
 });
 
+fruit.save()
 
 
 
-
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Insert some documents
-  collection.insertMany([{
-      a: 1
-    },
-    {
-      a: 2
-    },
-    {
-      a: 3
-    }
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-}
 
 const findDocuments = function(db, callback) {
   // Get the documents collection
