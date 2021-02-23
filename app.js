26270f7eefa492c208a8e1a706320fd6b56ed11d
fogset <zhangth1990@hotmail.com>
2021-02-22 19:41:11 -0800

    read data inside my database

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const fruitSchema = new mongoose.Schema({
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit({
-  name: "Apple",
-  rating: 7,
-  review: "Pretty solid as a fruit."
+  name: "Banana",
+  rating: 9,
+  review: "very nice fruit"
 });
 
-// fruit.save()
+//fruit.save()
 
 const personSchema = new mongoose.Schema({
     name: String,
@@ -31,17 +31,15 @@ const person = new newPerson({
   name: "John",
   age: 37
 });
-person.save();
+// person.save();
 
 
-const findDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Find some documents
-  collection.find({}).toArray(function(err, fruits) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits)
-    callback(fruits);
-  });
-}
+Fruit.find(function(err, fruits){
+  if(err){
+    console.log(err);
+  }else{
+    fruits.forEach(function(fruit){
+      console.log(fruit.name);
+    })
+  }
+})
